57cfe3b34cbfda827e6711d6c13d589148cf4555
vanshika-99 <54948141+vanshika-99@users.noreply.github.com>
2021-05-05 15:18:55 -0400

    fixed an error in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,31 +2,6 @@ import TestJS from './TestJs';
 import ConsoleLogIt from './ConsoleLogIt';
 import getJSON from './getJSON';
 
-TestJS();
-getJSON('', (data) => {
-  console.log(data);
-});
-
-getJSON('http://localhost:8000/api/v1/cities',
-  (err, records) => {
-    if (err !== null) {
-      alert(`Something went wrong: ${err}`);
-    } else {
-      const table = document.querySelector('table');
-      const data = Object.keys((records.data[0]));
-      const dataRecords = records.data;
-
-      generateTableHead(table, data);
-      generateTable(table, dataRecords);
-      /*
-            let data = Object.keys(records.data[0]);
-            generateTable(table, records.data); // generate the table first
-            generateTableHead(table, data); // then the head
-             */
-    }
-  });
-ConsoleLogIt('this workedss  in the bundle');
-
 function generateTableHead(table, data) {
   const thead = table.createTHead();
   const row = thead.insertRow();
@@ -50,3 +25,28 @@ function generateTable(table, data) {
     }
   }
 }
+
+TestJS();
+getJSON('', (data) => {
+  console.log(data);
+});
+
+getJSON('http://localhost:8000/api/v1/cities',
+  (err, records) => {
+    if (err !== null) {
+      alert(`Something went wrong: ${err}`);
+    } else {
+      const table = document.querySelector('table');
+      const data = Object.keys((records.data[0]));
+      const dataRecords = records.data;
+
+      generateTableHead(table, data);
+      generateTable(table, dataRecords);
+      /*
+            let data = Object.keys(records.data[0]);
+            generateTable(table, records.data); // generate the table first
+            generateTableHead(table, data); // then the head
+             */
+    }
+  });
+ConsoleLogIt('this worked in the bundle');
